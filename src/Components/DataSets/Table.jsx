4364c75de2e5a02c50312f889610617be04f75af
farazmahmood1 <faraz.m4765@gmail.com>
2024-04-24 21:23:43 +0500

    --core add the update user && edit user , updated gui

--- src/Components/DataSets/Table.jsx
+++ src/Components/DataSets/Table.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import "react-toastify/dist/ReactToastify.css";
+import { toast } from "react-toastify";
+import UpdateUser from '../Modals/UpdateUser';
+toast.configure();
 
 const Table = () => {
 
     const [data, setData] = useState([])
-    const [allData, setAllData] = useState([])
-
+    const [filteredTable, setFilteredTable] = useState([])
+    const [modalShow, setModalShow] = useState(false);
     const [pages, setPages] = useState(1);
     const [count, setCount] = useState('');
+    const [userId, setUserId] = useState(null)
     const resultsPerPage = 50;
 
     // function for getting page next:
@@ -34,8 +38,11 @@ const Table = () => {
         myHeaders.append("Cookie", "XSRF-TOKEN=eyJpdiI6IjI2ekFNNFlhVEZOSWlqazhoUUZpQ1E9PSIsInZhbHVlIjoiSklCdUpHTkUvUURnTDBwTmVkWi9obFpwQ202eUZwMFhmNFcyQkh0LzlFNnRJR1JmTk5UV1JvTE1VdlV5QmI5OUVMT1VsaFRVcU82V2lNMkI2dy9yTkY5WTVwaWZrUjd1ZkxBRDZaNmtrQ2IvY2FvUFFtaHJVVVNTQTdkV0VucVMiLCJtYWMiOiI2NGZhMmFmOTUyOGMwYWFjNjkzY2VlNDJiODBkNDljNmU0YTVlZDNmNDJhMWYxMGFmNDIwYWQ4OGFmZjMwMDE0IiwidGFnIjoiIn0%3D; laravel_session=eyJpdiI6IjRmL0pjbGh0K3JFaG1PeElDOTZqU3c9PSIsInZhbHVlIjoiYVk5TXRBUFF6RVdEeFhZN2VUWGxNeXphbWtlbjZFZWFIREp5Q05MOElpMFNrckMySUcwbGVxcFhWdmNZSmduUHJnWHh0R3ZoZXV0UXV2L1hNM1A1U3ZLWjZUaS90V2pyNFAwbXJCRldVeC9meDR4ODI1SHpqN1g4TWpBMFNLRWkiLCJtYWMiOiI4NDRjYjQ4ZDQ1YjE2NjM3ZDQ3OWQ2NzQyZjY5NDM0NTJlY2E0ZWE4NzM3N2YwMWJkZjk1Njk2Y2NlOTZiYjE3IiwidGFnIjoiIn0%3D");
 
         const formdata = new FormData();
-        if (attr === "age") {
-            formdata.append("age", e.target.value);
+        if (attr === "fromAge" && attr === "toAge") {
+            const fromAge = document.getElementById("fromAge").value;
+            const toAge = document.getElementById("toAge").value;
+            const ageRange = `${fromAge}-${toAge}`;
+            formdata.append("age_range", ageRange);
         }
         else if (attr === "country") {
             formdata.append("country", e.target.value);
@@ -62,13 +69,37 @@ const Table = () => {
             .then((result) => {
                 setData(result.data)
                 setCount(result.total_count)
-                // console.log(result)
-
+                setFilteredTable(result.data)
             })
             .catch((error) => console.error(error));
     }
 
-    const exportData = () => {
+    const deleteUser = (id) => {
+        const requestOptions = {
+            method: "POST",
+            redirect: "follow"
+        };
+
+        fetch(`https://apis.reportsxapis.com/api/delete_general_data_by_id/${id}`, requestOptions)
+            .then((response) => response.json())
+            .then((result) => {
+                if (result?.status === "200") {
+                    toast.success("User deleted successfully")
+                    setInterval(() => {
+                        window.location.reload()
+                    }, 1500);
+                }
+                else if (result?.status === "401") {
+                    toast.warn(result?.message)
+                }
+            })
+            .catch((error) => {
+                console.error(error)
+                toast.danger("Something went wrong...")
+            });
+    }
+
+    const exportAllData = () => {
         const myHeaders = new Headers();
         myHeaders.append("Accept", "application/json");
         myHeaders.append("Authorization", "Bearer 8|JE5SHg4ESU5vBkiwpdGp4Fzli7dpulcsrlLbfVPz18b952af");
@@ -83,16 +114,19 @@ const Table = () => {
         fetch(`https://apis.reportsxapis.com/api/get_general_data`, requestOptions)
             .then((response) => response.json())
             .then((result) => {
-                setAllData(result.data)
                 exportCSV(result.data)
             })
             .catch((error) => console.error(error));
     }
 
     const exportCSV = (data) => {
-        const filteredData = data.map(({ Idate, created_at, updated_at, ...rest }) => rest);
-        const csvContent = "data:text/csv;charset=utf-8," +
-            filteredData.map((item) => Object.values(item).join(",")).join("\n");
+        const filteredData = data.map(({ id, Idate, created_at, updated_at, ...rest }) => rest);
+        const columnOrder = ['email', 'name', 'dob', 'age', 'gender', 'goal', 'country'];
+        const csvContent =
+            "data:text/csv;charset=utf-8," +
+            [columnOrder.join(',')].concat(
+                filteredData.map((item) => columnOrder.map((key) => item[key]).join(","))
+            ).join("\n");
         const encodedUri = encodeURI(csvContent);
         const link = document.createElement("a");
         link.setAttribute("href", encodedUri);
@@ -101,48 +135,64 @@ const Table = () => {
         link.click();
     };
 
+    const exportFilteredCSV = () => {
+        const filteredData = filteredTable.map(({ Idate, created_at, updated_at, ...rest }) => rest);
+        // const csvContent = "data:text/csv;charset=utf-8," +
+        //     filteredData.map((item) => Object.values(item).join(",")).join("\n");
+
+        const columnOrder = ['email', 'name', 'dob', 'age', 'gender', 'goal', 'country'];
+        const csvContent =
+            "data:text/csv;charset=utf-8," +
+            [columnOrder.join(',')].concat(
+                filteredData.map((item) => columnOrder.map((key) => item[key]).join(","))
+            ).join("\n");
+
+
+        const encodedUri = encodeURI(csvContent);
+        const link = document.createElement("a");
+        link.setAttribute("href", encodedUri);
+        link.setAttribute("download", "data.csv");
+        document.body.appendChild(link);
+        link.click();
+    };
+
+    const modalClose = (items) => {
+        setModalShow((prev) => !prev)
+        setUserId(items)
+    }
 
     return (
         <div>
-
             <div className="row">
                 <div className="col-lg-12">
                     <div className="card m-lg-5">
                         <div className="card-body">
-                            <h3 className="card-title">Special title treatment</h3>
-                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <h3 className="card-title">Report <i className='fa-thin fa-x' /></h3>
+                            <p className="card-text">Efficiently send multiple emails at once with our website, streamlining communication and saving you valuable time and effort.</p>
                             <div className='d-flex justify-content-right'></div>
-                            <a className="btn btn-outline-success mt-2 mb-3" onClick={exportData}>Export All CSVs</a>
-                            <a className="btn btn-outline-success mt-2 mb-3 ms-2" onClick={exportData}>Export Filtered CSVs</a>
-
+                            <a className="btn btn-outline-success mt-2 mb-3" onClick={exportAllData}>Export All CSVs</a>
+                            <a className="btn btn-outline-success mt-2 mb-3 ms-2" onClick={exportFilteredCSV}>Export Filtered CSVs</a>
                             <div className='row '>
                                 <div className='col-lg-3 p-0 m-0' >
-                                    <input className='form-control' onChange={(e) => getData(e, "age")} placeholder='Enter Age' style={{ borderRadius: "15px" }} type="text" />
+
+                                    <div className='d-flex'>
+                                        <input className='form-control' id='fromAge' onChange={(e) => getData(e, "fromAge")} placeholder='From Age' style={{ borderRadius: "15px 0px 0px 15px", borderRight: "none" }} type="number" />
+                                        <input className='form-control' id='toAge' onChange={(e) => getData(e, "toAge")} placeholder='To Age' style={{ borderRadius: "0px 15px 15px 0px", borderLeft: "none" }} type="number" />
+                                    </div>
+
                                 </div>
                                 <div className='col-lg-3 p-0 m-0' >
-
                                     <select className="form-select" onChange={(e) => getData(e, "gender")} style={{ borderRadius: "15px" }} aria-label="Default select example">
                                         <option value="">Select Gender</option>
                                         <option value="male">Male</option>
                                         <option value="female">Female</option>
                                     </select>
-
-
-                                    {/* <input className='form-control' onChange={(e) => getData(e, "gender")} placeholder='Enter Gender' style={{ borderRadius: "15px" }} type="text" /> */}
                                 </div>
                                 <div className='col-lg-3 p-0 m-0' >
                                     <input className='form-control' onChange={(e) => getData(e, "goal")} placeholder='Enter Goals' style={{ borderRadius: "15px" }} type="text" />
                                 </div>
-
                                 <div className='col-lg-3 p-0 m-0' >
-
-                                    <select className="form-select" onChange={(e) => getData(e, "country")} style={{ borderRadius: "15px" }} aria-label="Default select example">
-                                        <option value="">Select Gender</option>
-                                        <option value="male">Male</option>
-                                        <option value="female">Female</option>
-                                    </select>
-
-                                    {/* <input className='form-control' onChange={(e) => getData(e, "country")} placeholder='Enter Country' style={{ borderRadius: "15px" }} type="text" /> */}
+                                    <input className='form-control' onChange={(e) => getData(e, "country")} placeholder='Enter Country' style={{ borderRadius: "15px" }} type="text" />
                                 </div>
                             </div>
 
@@ -157,6 +207,8 @@ const Table = () => {
                                         <th>Gender</th>
                                         <th>Goal</th>
                                         <th>Country</th>
+                                        <th>Edit</th>
+                                        <th>Delete</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -165,14 +217,16 @@ const Table = () => {
                                             return (
                                                 <>
                                                     <tr>
-                                                        <td>{items.id}</td>
-                                                        <td>{items.email}</td>
-                                                        <td>{items.name}</td>
-                                                        <td>{items.dob}</td>
-                                                        <td>{items.age}</td>
-                                                        <td>{items.gender}</td>
-                                                        <td>{items.goal}</td>
-                                                        <td>{items.country}</td>
+                                                        <td>{items?.id}</td>
+                                                        <td>{items?.email}</td>
+                                                        <td>{items?.name}</td>
+                                                        <td>{items?.dob}</td>
+                                                        <td>{items?.age}</td>
+                                                        <td>{items?.gender}</td>
+                                                        <td>{items?.goal}</td>
+                                                        <td>{items?.country}</td>
+                                                        <td><button className='btn btn-outline-success' onClick={() => modalClose(items)}><i className='fa fa-solid fa-marker' /></button></td>
+                                                        <td><button className='btn btn-outline-danger' onClick={() => deleteUser(items?.id)}><i className='fa fa-solid fa-trash' /></button></td>
                                                     </tr>
                                                 </>
                                             )
@@ -182,7 +236,7 @@ const Table = () => {
 
                                 </tbody>
                                 <tfoot>
-                                    <div className="p-3">
+                                    <div className="container mt-5">
                                         <button className="btn btn-outline-success btn-sm" onClick={handlePrevPage} disabled={pages === 1}>
                                             <i className="fa-solid fa-arrow-left"></i>
                                         </button>
@@ -190,13 +244,14 @@ const Table = () => {
                                         <button className="btn btn-outline-success btn-sm" onClick={handleNextPage} disabled={totalResults <= endResult}>
                                             <i className="fa-solid fa-arrow-right"></i>
                                         </button>
-                                        <p >Showing {startResult} - {count}  results  -  total :&nbsp;&nbsp;{count}</p>
+                                        <p>Showing {startResult} - {count}  results  -  total :&nbsp;&nbsp;{count}</p>
                                     </div>
-
                                 </tfoot>
                             </table>
 
-
+                            {
+                                modalShow === true ? <UpdateUser userId={userId} modalShow={modalShow} modalClose={modalClose} /> : null
+                            }
                         </div>
                     </div>
                 </div>
